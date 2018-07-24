aab27b094b8868474f107b2085ab2dbd2f4cde9b
kunal-mohta <kunalmohta1818@gmail.com>
2018-07-24 10:42:28 +0530

    Classic Problem - Towers of Hanoi

---
+++ classicProblems/towersOfHanoi.cpp
@@ -0,0 +1,77 @@
+// Classic problem "Towers of Hanoi" with stacks
+// Concepts - Recursion + Stacks
+
+#include<iostream>
+
+// Stack impl
+class StackNode {
+  public:
+    int val;
+    StackNode *next = nullptr;
+
+    StackNode (int v) {
+      val = v;
+    }
+};
+
+class Stack {
+  public:
+    StackNode *top = nullptr;
+
+    void push (int value) {
+      if (top == nullptr) {
+        top = new StackNode(value);
+      }
+      else {
+        StackNode* newNode = new StackNode(value);
+        newNode->next = top;
+        top = newNode;
+      }
+    }
+
+    int pop () {
+      int returnVal = top->val;
+      top = top->next;
+      return returnVal;
+    }
+
+    void print () {
+      StackNode* temp = top;
+
+      while (temp) {
+        std::cout<<temp->val<<std::endl;
+        temp = temp->next;
+      }
+    }
+};
+
+// function of value
+void moveNodesFromTo (int num, Stack *source, Stack *target, Stack *missing) {
+  if (num == 1) {
+    target->push(source->pop());
+  }
+  else if (num == 2) {
+    missing->push(source->pop());
+    target->push(source->pop());
+    target->push(missing->pop());
+  }
+  else {
+    moveNodesFromTo(num - 1, source, missing, target);
+    moveNodesFromTo(1, source, target, missing);
+    moveNodesFromTo(num-1, missing, target, source);
+  }
+}
+
+int main () {
+  Stack A, B, C;
+
+  A.push(5);
+  A.push(4);
+  A.push(3);
+  A.push(2);
+  A.push(1);
+
+  moveNodesFromTo(5, &A, &C, &B);
+  C.print();
+  // A.print();
+}
